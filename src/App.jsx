9af503176937ca7e2fb0635639f44a2ba4005287
walid885 <bettaieb88walid@gmail.com>
2024-06-07 16:09:49 +0100

    renaming style.js en styles.js and the initalization of navbar.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ const  App = () =>  {
       <Navbar />
       <Hero />
     </div>
+    <About />
+    <Experience />
+    <Tech />
+    <Works />
+    <Feedbacks />
+    <div className="relative z-0">
+      <Contact /> 
+      <StarsCanvas />
+
+    </div>
     </div>
     </BrowserRouter>
     )
