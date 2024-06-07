b8da6014932f8c131a8671eb68e8f38638f43b41
walid885 <bettaieb88walid@gmail.com>
2024-06-07 15:52:24 +0100

    initalization of the page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
+import { BrowserRouter  } from "react-router-dom" ;
+import{ About, Contact, Experience, Feedbacks , Hero, Navbar , Tech, Works, StarsCanvas} from "./components"
 const  App = () =>  {
 
   return (
-   <div>Bettaieb Walid</div>
-  )
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+    <div className="bg-hero-pattern  bg-cover bg-no-repeat bg-center">
+      <Navbar />
+      <Hero />
+    </div>
+    </div>
+    </BrowserRouter>
+    )
 }
 
 export default App
