81424ce7879ce399e586540b2e52eb56eb80ed18
walid885 <bettaieb88walid@gmail.com>
2024-06-07 16:40:29 +0100

    solving the error, the issue was that the Link component shall have an L , instead of l , issue solved , initial version of navbar is working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React ,{useEffect , useState} from 'react' ;
 import { Link  } from 'react-router-dom' ;
-import {styles  } from '../styles'
+import {styles  } from '../styles.js'
 import { navLinks } from  '../constants'
 import {logo , menu ,close} from '../assets'
 
 const Navbar = () => {
+  const [active, setActive ] = useState("")
   return (
-    <div>Navbar</div>
-  )
+    <nav className={`
+      ${styles.paddindX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary  `}>
+        <div className='w-full flex justify-between items-center max-w-7xl mx-auto' >
+          <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(" ");
+        window.scrollTo(0,0);
+        }}> 
+          <img src={logo} alt='Logo'   className='w-9 h-9 object-contain'/>
+          </Link>
+        </div>
+    </nav>
+    )
 }
 
 export default Navbar
