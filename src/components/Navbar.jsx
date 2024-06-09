2ecda1401b4decac6991e52ababdd2abb30e8290
walid885 <bettaieb88walid@gmail.com>
2024-06-09 23:58:25 +0100

    setting up the logo in the tab , as well , inital stayling of the logo page in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
         window.scrollTo(0,0);
         }}> 
           <img src={logo} alt='Logo'   className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold cursor-pointer'> Walid Bettaieb | computer Engineering student </p>
           </Link>
         </div>
     </nav>
