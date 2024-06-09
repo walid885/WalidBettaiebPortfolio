22d810e4dcdf4aac794756b6dfb5d471bb9afee4
walid885 <bettaieb88walid@gmail.com>
2024-06-10 00:52:37 +0100

    there is an error in the customazation and adding content to the toggle in the small devices , further debugging needed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,6 +44,35 @@ const Navbar = () => {
               className='w-[28px] h-[28px] object-contain cursor-pointer'
               onClick={() => setToggle (!toggle)}
               />
+              <div className={`${!toggle ? 'hidden' : flex} 
+              p-6 black-gardient absolute 
+              top-20 right-0 mx-4 
+              my-2 min-w[140px] z-10 rounded-x1
+              
+              
+              
+              `}>
+
+<ul className='list-none flex  justify-end items-start flex-col gap-4'>
+
+{navLinks.map((link) => (
+  <li
+  key = {link.id}
+  className={
+
+    `${
+      active === link.title 
+      ? "text-white"
+      :'text-secondary'
+    } hover:text-white text-[18px] font-medium cursor-pointer`}
+    onClick={() => setActive(link.title)}>
+    <a href={`#${link.id}`}>{link.title} </a>
+  </li>
+))}
+
+</ul>
+
+              </div>
           </div>
         </div>
     </nav>
