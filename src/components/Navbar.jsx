94dbf4d1a12babc49ad1013eca45436be6f6f242
walid885 <bettaieb88walid@gmail.com>
2024-06-10 15:02:05 +0100

    navbar component part finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
 
   return (
     <nav className={`
-      ${styles.paddindX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary  `}>
+      ${styles.paddingX}  w-full flex items-center py-5 fixed top-0 z-20 bg-primary  `}>
         <div className='w-full flex justify-between items-center max-w-7xl mx-auto' >
           <Link to="/" className="flex items-center gap-2" onClick={() => { setActive(" ");
         window.scrollTo(0,0);
         }}> 
           <img src={logo} alt='Logo'   className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'> Walid Bettaieb <span className= "sm:block hidden"> |  computer Engineering student </span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'> Walid Bettaieb &nbsp;<span className= "sm:block hidden"> | Computer Engineering Student </span></p>
           </Link>
           <ul className='list-none hidden sm:flex flex-row gap-10'>
 
@@ -63,9 +63,12 @@ const Navbar = () => {
     `${
       active === link.title 
       ? "text-white"
-      :'text-secondary'
-    } hover:text-white text-[18px] font-medium cursor-pointer`}
-    onClick={() => setActive(link.title)}>
+      :"text-secondary"
+    }font-poppins font-medium cursor-pointer text-[16px]`}
+    onClick={() =>  {
+      setToggle(!toggle);
+      setActive(link.title);}
+    }>
     <a href={`#${link.id}`}>{link.title} </a>
   </li>
 ))}
