a833c338a5a15f626f54fac9a0fca1c0f491b7c1
walid885 <bettaieb88walid@gmail.com>
2024-06-11 12:34:53 +0100

    finishing the about section, and using a higher order component for wrappping the other compoennts , and applying the styling for once

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -4,8 +4,17 @@ import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 const SectionWrapper = (Component, idName) => function HOC(){
 return (
-    <motion.section>
-    <component />
+    <motion.section
+    variants={staggerContainer()}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once:true, amount: 0.25}}
+    className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+    >
+        <span className=" hash-span " id={idName}>
+            &nbsp;
+        </span>
+    <Component />
 
     </motion.section>
 )
