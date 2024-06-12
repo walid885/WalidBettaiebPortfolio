073aef681214dfd7905c7669851e0ed17abaada8
walid885 <bettaieb88walid@gmail.com>
2024-06-13 00:21:34 +0100

    for the automatic movement of the balls , the values need to be more accurate and configured

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,7 +14,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={2.75} rotationIntensity={2} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
