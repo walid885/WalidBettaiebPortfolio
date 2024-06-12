c78b7c2a7c14a70503a66e08a26bca5df9b2b4db
walid885 <bettaieb88walid@gmail.com>
2024-06-12 19:18:03 +0100

    solving that small error in the cards containers

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -12,6 +12,7 @@ return (
     className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
         <span className=" hash-span " id={idName}>
+
             &nbsp;
         </span>
     <Component />
