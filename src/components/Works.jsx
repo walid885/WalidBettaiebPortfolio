1215efac585d194ddf4492fd7e500964c26628d5
walid885 <bettaieb88walid@gmail.com>
2024-06-13 15:37:42 +0100

    setting up the # under each card of projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -48,16 +48,28 @@ className="w-1/2 h-1/2 object-contain "/>
 </div>
 </div>
 <div className='mt-5'>
-<h3>{name}</h3>
-<p>{description}</p>
+<h3 className="text-white font-bold text-[24px]">{name}</h3>
+<p className="mt-2 text-secondary text-[14px]">{description}</p>
+
+      <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
 
 </div>
+
 
 </Tilt>
 
     </motion.div>
   )
-}
+} 
 
 
 
