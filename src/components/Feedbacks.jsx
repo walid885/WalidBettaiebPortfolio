7c4a8345c6d34c7f6180fb77818466734452263c
walid885 <bettaieb88walid@gmail.com>
2024-06-13 16:35:47 +0100

    setting up the testionlas texts

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -7,11 +7,36 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { testimonials } from '../constants'
 
 
+const FeedbackCard = ({index, testimonial , name, designation, company, image}) => (
+  <motion.div>
+
+  <p>{testimonial}</p>
+
+  </motion.div>
+)
+
 
 const Feedbacks = () => {
   return (
     <div className="mt-12 bg-black-100 rounded-20px">
-      
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`} >
+<motion.div variants={textVariant()}>
+  <p className={styles.sectionSubText}> What others say</p>
+  <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+</motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7 `}> 
+
+        {testimonials.map((testimonial, index) => 
+        (
+          <FeedbackCard 
+          key={testimonial.name}
+          index={index}
+          {...testimonial}
+          />
+        ))}
+
+      </div>
     </div>
 
     )
