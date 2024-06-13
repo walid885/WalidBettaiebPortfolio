b8f3c91497ef2ba1c376889663768f0977da0cf3
walid885 <bettaieb88walid@gmail.com>
2024-06-13 16:05:18 +0100

    initial commit of Feedback part

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import {motion} from "framer-motion"
+import { styles } from '../styles'
+
+import { SectionWrapper } from '../hoc'
+import { fadeIn, textVariant } from '../utils/motion'
+import { testimonials } from '../constants'
+
+
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
-  )
+    <div className="mt-12 bg-black-100 rounded-20px">
+      
+    </div>
+
+    )
 }
 
 export default Feedbacks
