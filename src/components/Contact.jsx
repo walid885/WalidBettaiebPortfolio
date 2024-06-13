9058c83177a512659a760fb10c7f1894528deebf
walid885 <bettaieb88walid@gmail.com>
2024-06-13 20:04:10 +0100

    styling finished of the contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -52,27 +52,34 @@ const handleSubmit = (e) => {}
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
+            <span className='text-white font-medium mb-4'>Your Email</span>
             <input
-              type='text'
-              name='name'
-              value={form.name}
+              type='email'
+              name='email'
+              value={form.email}
               onChange={handleChange}
-              placeholder="What's your name?"
+              placeholder="What's your email?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
           <label className='flex flex-col'>
-            <span className='text-white font-medium mb-4'>Your Name</span>
-            <input
-              type='text'
-              name='name'
-              value={form.name}
+            <span className='text-white font-medium mb-4'>Your Message</span>
+            <textarea
+              rows={7}
+              name='message'
+              value={form.message}
               onChange={handleChange}
-              placeholder="What's your name?"
+              placeholder="What do you want to say?"
               className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium'
             />
           </label>
+            <button
+              type="submit"
+              className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary  rounded-xl"
+            >
+
+                {loading? 'sending ... ' :'send'}
+            </button>
 
         </form>
 
