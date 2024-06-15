204256050dcd87e684ec2c7ed56812bfd9ccbdb9
walid885 <bettaieb88walid@gmail.com>
2024-06-15 20:20:13 +0100

    finished the last tasks , project done , and deleting the computer canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,9 +23,10 @@ const Hero = () => {
 </div>
 </div>
 
+ {/* <Feedbacks />
 
 
-{<ComputersCanvas />}
+{<ComputersCanvas />} */}
 <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
     <a href='#about'> 
     <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
